66a519c25b07b7d199b369e0c6558e7b5b00a3bd
nguyen92-dev <nguyennd92.dev@gmail.com>
2025-08-08 14:45:03 +0700

    refactor naming

--- internal/usecase/dto/base_response.go
+++ internal/usecase/dto/base_response.go
@@ -2,24 +2,24 @@ package dto
 
 type APIResponse[TResponse any] struct {
 	Status   int       `json:"status"`
-	Sucess   bool      `json:"sucess"`
-	Data     TResponse `json:"data"`
-	Error    APIError  `json:"error"`
-	Metadata any       `json:"metadata"`
+	Success  bool      `json:"success"`
+	Data     TResponse `json:"data,omitempty"`
+	Error    APIError  `json:"error,omitempty"`
+	Metadata any       `json:"metadata,omitempty"`
 }
 
 type APIError struct {
 	ErrorType string `json:"type"`
 	Message   string `json:"message"`
-	Detail    any    `json:"detail"`
+	Detail    any    `json:"detail,omitempty"`
 }
 
 func SuccessResponse[TResponse any](status int, data TResponse) APIResponse[TResponse] {
-	return APIResponse[TResponse]{Status: status, Data: data}
+	return APIResponse[TResponse]{Status: status, Data: data, Success: true}
 }
 
 func ErrorResponse(status int, err APIError) APIResponse[any] {
-	return APIResponse[any]{Status: status, Error: err}
+	return APIResponse[any]{Status: status, Error: err, Success: false}
 }
 
 func NewAPIError(errType string, message string, details any) APIError {
