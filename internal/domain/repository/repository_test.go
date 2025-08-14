3019127838ddeafc1a298ddd0a9ec8487c0d0dfc
nguyen92-dev <nguyennd92.dev@gmail.com>
2025-08-14 23:31:43 +0700

    unit test size reposiory

--- internal/domain/repository/repository_test.go
+++ internal/domain/repository/repository_test.go
@@ -86,6 +86,7 @@ func TestCategoryCreateOK(t *testing.T) {
 
 func TestCategoryFindByIdOK(t *testing.T) {
 	category := mockCategory()
+	category.ID = 1
 	gormdb, mock, cleanup := newGormWithMock(t)
 	categoryRepo := mockCategoryRepository(gormdb)
 	defer cleanup()
@@ -159,6 +160,172 @@ func TestDeleteCategoryOK(t *testing.T) {
 	mock.ExpectCommit()
 
 	err := categoryRepo.DeleteEntity(gormdb, category)
+	require.NoError(t, err)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func mockSizeRepository(gdb *gorm.DB) SizeRepository {
+	var preloads []database.PreloadEntity
+	return newBaseRepository[model.Size](gdb, preloads)
+}
+
+func mockSize() model.Size {
+	return model.Size{
+		BaseModel: model.BaseModel{
+			CreatedBy: "NguyenND",
+			UpdatedBy: "NguyenND",
+		},
+		Name:       "16cm",
+		CategoryID: 2,
+	}
+}
+
+func TestCreateSizeOk(t *testing.T) {
+	size := mockSize()
+	gormdb, mock, cleanup := newGormWithMock(t)
+	sizeRepo := mockSizeRepository(gormdb)
+	defer cleanup()
+
+	q := `INSERT INTO "sizes" ("created_by","updated_by","name","category_id") VALUES ($1,$2,$3,$4) RETURNING "id"`
+	insertSQL := strings.ReplaceAll(regexp.QuoteMeta(q), " ", `\s+`)
+
+	mock.ExpectBegin()
+	mock.ExpectQuery(insertSQL).
+		WithArgs(size.CreatedBy, size.UpdatedBy, size.Name, size.CategoryID).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectCommit()
+
+	entity, err := sizeRepo.Create(gormdb, size)
+	require.NoError(t, err)
+	require.NotNil(t, entity)
+	require.NotNil(t, entity.ID)
+	require.NoError(t, mock.ExpectationsWereMet())
+
+}
+
+func TestCreateSizeWithInvalidCategoryID(t *testing.T) {
+	size := mockSize()
+	gormdb, mock, cleanup := newGormWithMock(t)
+	sizeRepo := mockSizeRepository(gormdb)
+	defer cleanup()
+
+	q := `INSERT INTO "sizes" ("created_by","updated_by","name","category_id") VALUES ($1,$2,$3,$4) RETURNING "id"`
+	insertSQL := strings.ReplaceAll(regexp.QuoteMeta(q), " ", `\s+`)
+
+	mock.ExpectBegin()
+	mock.ExpectQuery(insertSQL).
+		WithArgs(size.CreatedBy, size.UpdatedBy, size.Name, size.CategoryID).
+		WillReturnError(gorm.ErrForeignKeyViolated)
+	mock.ExpectRollback()
+
+	_, err := sizeRepo.Create(gormdb, size)
+	require.Error(t, err)
+	require.ErrorIs(t, err, gorm.ErrForeignKeyViolated)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestFindSizeByIDOK(t *testing.T) {
+	size := mockSize()
+	gormdb, mock, cleanup := newGormWithMock(t)
+	sizeRepo := mockSizeRepository(gormdb)
+	defer cleanup()
+
+	q := `SELECT * FROM "sizes" WHERE id = $1 ORDER BY "sizes"."id" LIMIT $2`
+	querySql := strings.ReplaceAll(regexp.QuoteMeta(q), " ", `\s+`)
+	cols := []string{"id", "created_by", "updated_by", "name", "category_id"}
+
+	mock.ExpectQuery(querySql).
+		WithArgs(size.ID, 1).
+		WillReturnRows(sqlmock.NewRows(cols).
+			AddRow(size.ID, size.CreatedBy, size.UpdatedBy, size.Name, size.CategoryID))
+
+	entity, err := sizeRepo.FindById(gormdb, size.ID)
+	require.NoError(t, err)
+	require.NotNil(t, entity)
+	require.Equal(t, size, entity)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestFindSizeByIDError(t *testing.T) {
+	gormdb, mock, cleanup := newGormWithMock(t)
+	sizeRepo := mockSizeRepository(gormdb)
+	defer cleanup()
+
+	q := `SELECT * FROM "sizes" WHERE id = $1 ORDER BY "sizes"."id" LIMIT $2`
+	querySql := strings.ReplaceAll(regexp.QuoteMeta(q), " ", `\s+`)
+	cols := []string{"id", "created_by", "updated_by", "name", "category_id"}
+
+	mock.ExpectQuery(querySql).
+		WithArgs(100, 1).
+		WillReturnRows(sqlmock.NewRows(cols))
+
+	_, err := sizeRepo.FindById(gormdb, 100)
+	require.Error(t, err)
+	require.ErrorIs(t, err, gorm.ErrRecordNotFound)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUpdateSizeOK(t *testing.T) {
+	size := mockSize()
+	size.ID = 100
+	gormdb, mock, cleanup := newGormWithMock(t)
+	sizeRepo := mockSizeRepository(gormdb)
+	defer cleanup()
+
+	q := `UPDATE "sizes" SET .* WHERE id = \$5 RETURNING .*`
+	updateSQL := regexp.MustCompile(q).String() // cho rõ là regex
+
+	mock.ExpectBegin()
+	mock.ExpectQuery(updateSQL).
+		WithArgs(size.CreatedBy, size.UpdatedBy, size.Name, size.CategoryID, 100).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_by", "updated_by", "name", "category_id"}).
+			AddRow(size.ID, size.CreatedBy, size.UpdatedBy, size.Name, size.CategoryID))
+	mock.ExpectCommit()
+
+	entity, err := sizeRepo.Update(gormdb, 100, size)
+	require.NoError(t, err)
+	require.Equal(t, size, entity)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUpdateSizeError(t *testing.T) {
+	size := mockSize()
+	size.ID = 100
+	gormdb, mock, cleanup := newGormWithMock(t)
+	sizeRepo := mockSizeRepository(gormdb)
+	defer cleanup()
+
+	q := `UPDATE "sizes" SET .* WHERE id = \$5 RETURNING .*`
+	updateSQL := regexp.MustCompile(q).String() // cho rõ là regex
+
+	mock.ExpectBegin()
+	mock.ExpectQuery(updateSQL).
+		WithArgs(size.CreatedBy, size.UpdatedBy, size.Name, size.CategoryID, 100).
+		WillReturnError(gorm.ErrForeignKeyViolated)
+	mock.ExpectRollback()
+
+	_, err := sizeRepo.Update(gormdb, 100, size)
+	require.Error(t, err)
+	require.ErrorIs(t, err, gorm.ErrForeignKeyViolated)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestDeleteSizeOK(t *testing.T) {
+	size := mockSize()
+	size.ID = 100
+	gormdb, mock, cleanup := newGormWithMock(t)
+	sizeRepo := mockSizeRepository(gormdb)
+	defer cleanup()
+
+	q := `DELETE FROM "sizes" WHERE "sizes"."id" = $1`
+	deleteSQL := strings.ReplaceAll(regexp.QuoteMeta(q), " ", `\s+`)
+
+	mock.ExpectBegin()
+	mock.ExpectExec(deleteSQL).
+		WillReturnResult(sqlmock.NewResult(100, 1))
+	mock.ExpectCommit()
+
+	err := sizeRepo.DeleteEntity(gormdb, size)
 	require.NoError(t, err)
 	require.NoError(t, mock.ExpectationsWereMet())
 }
